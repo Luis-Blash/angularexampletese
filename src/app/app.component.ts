84f90066de3018af135dbc003523936dcec14116
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2021-03-25 13:32:40 -0600

    Se agregoel primer componente a app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // Darle un nombre al componente
   selector: 'app-root',
+  // Donde va estar
   templateUrl: './app.component.html',
+  // El css de mi componete
   styleUrls: ['./app.component.css']
 })
+// Variables que se pueden utilizar en el html
 export class AppComponent {
   title = 'practica1';
 }
