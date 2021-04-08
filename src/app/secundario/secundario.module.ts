dd1e7da469d36d3d760a8dbb73fbcd4484740c45
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2021-04-08 00:38:21 -0500

    Se creo los modulos de mi barra de navegacion, Ayuda, Acerca, Documentos

--- src/app/secundario/secundario.module.ts
+++ src/app/secundario/secundario.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselComponent } from './carousel/carousel.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AyudaComponent } from './ayuda/ayuda.component';
+import { AcercaComponent } from './acerca/acerca.component';
+import { DocumentosComponent } from './documentos/documentos.component';
 
 
 
 @NgModule({
-  declarations: [CarouselComponent, NavbarComponent],
-  exports:[CarouselComponent,NavbarComponent],
+  declarations: [CarouselComponent, NavbarComponent, AyudaComponent, AcercaComponent, DocumentosComponent],
+  exports:[CarouselComponent,NavbarComponent, AyudaComponent, AcercaComponent, DocumentosComponent],
   imports: [
     CommonModule
   ]
